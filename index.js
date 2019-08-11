5059b868e81efb0337c63c61ba67ebb1983c4f05
rajk1121 <rajat.mishra1121@gmail.com>
2019-08-11 21:37:08 +0530

    v1.5

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = require('./app.js');
 const fs = require('fs');
 const str = fs.readFileSync('./template/index.html').toString();
 const propRouter = require('./Router/property');
+var cors = require('cors');
 const DB = "mongodb+srv://rajk1121:Rajat1121@cluster0-chamy.mongodb.net/test?retryWrites=true&w=majority";
 const port = process.env.PORT || 80;
 mongoose.connect(DB, {
@@ -11,6 +12,7 @@ mongoose.connect(DB, {
 }).then(conn => {
     console.log('Connnected to DataBase');
 });
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json())
 app.get(['/home', '/'], (req, res) => {
